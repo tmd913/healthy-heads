03bad13dd6f813012495cd68a5ac5d35cd4659a1
Irene R <irene@smallwondermedia.com>
2018-10-12 10:37:27 -0400

    blue

---
+++ public/assets/img/The Coding Portfolio of Irene Rojas_files/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $("#project1Trigger").on("click", function() {
+
+        $("#project1Modal").addClass("show-modal");
+
+        
+    });
+
+
+
+
+
+    $("#project2Trigger").on("click", function() {
+
+        $("#project2Modal").addClass("show-modal");
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
